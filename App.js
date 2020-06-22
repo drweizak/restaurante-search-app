c620aded68aac9ae05588ec10ce8a140d9498e46
André Ferreira <andreferreira.coder@gmail.com>
2020-06-22 14:24:51 +0200

    clean

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 export default function App() {
   return (
